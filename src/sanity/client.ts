94779896f0e0f4afd8af5686d6cb5f11582678f8
riyanshlunia <riyansh.lunia20@gmail.com>
2026-08-05 19:17:19 +0530

    Fixed the font issue

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -4,16 +4,16 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
 
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion, 
-  useCdn: false,
-})
+// export const client = createClient({
+//   projectId,
+//   dataset,
+//   apiVersion, 
+//   useCdn: false,
+// })
 
 
-// export const client = {
-//   fetch: async () => {
-//     return []; // return empty mock data for now
-//   }
-// };
+export const client: any = {
+  fetch: async () => {
+    return []; // return empty mock data for now
+  }
+};
